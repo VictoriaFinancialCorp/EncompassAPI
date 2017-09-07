0bd7cfd262fb9d66b3c6e515b60a77d94c944c3d
Calvin Thanh <calvin@victoriafinancial.local>
2017-09-06 18:16:16 -0700

    logins

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use(express.static('public'));
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var config = require('config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login/login', { title: 'Login' , clientID: config.get('EncompassAPI.api_client_id')});
+});
+
+router.get('/login0', function(req, res, next) {
+  res.render('login/login0', { title: 'Login' });
 });
 
 module.exports = router;
