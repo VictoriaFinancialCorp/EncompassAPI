5b6df1226a9209da6c2bf1d70549534819087863
Calvin Thanh <calvin@victoriafinancial.local>
2017-10-25 16:31:06 -0700

    universal table

--- controllers/ReportController.js
+++ controllers/ReportController.js
@@ -1,11 +1,13 @@
 
 var config = require('config');
 var https = require('https');
+var numeral = require('numeral');
 
 module.exports = {
 
   getFundedFiles: function(req, res, next) {
 
+    //check if logged in
     if(typeof req.session.token === 'undefined' || typeof req.session.token['access_token'] === 'undefined' ){
       console.log('not logged in');
       return res.json('Please log in');
@@ -21,11 +23,7 @@ module.exports = {
       }
     };
 
-    //console.log(options);
-
-    var data = "";
-
-    let body= {
+    let body = {
       "filter": {
         "operator": "or",
         "terms": [
@@ -49,7 +47,7 @@ module.exports = {
       ]
     }
 
-    //console.log(body);
+    var data = "";
 
     var httpsReq = https.request(options, function(response) {
       response.setEncoding('utf8');
@@ -60,7 +58,12 @@ module.exports = {
       response.on('end', function(){
         //console.log("response: " + data);
         //return res.json(JSON.parse(data));
-        return res.render('reports/fundedFiles', {data: JSON.parse(data) } );
+        //return res.render('reports/fundedFiles', {data: JSON.parse(data) } );
+        return res.render('reports/tableReport', {
+          title: "Funded Files",
+          header: ["Inestor", "Investor Number", "Loan Number", "Borrower Name", "Loan Amount", "Interest Rate", "Loan Term", "Processor", "Loan Officer"],
+          rows: parseData(JSON.parse(data), body['fields'])
+        } );
       });
     });
     httpsReq.on('error', function(e) {
@@ -70,6 +73,31 @@ module.exports = {
     // write data to request body
     httpsReq.write(JSON.stringify(body));
     httpsReq.end();
+
+    function parseData(data, fields){
+      let outputArray = [];
+      data.forEach(function(row){
+        row = row.fields;
+        let rowArray = [
+          row["Fields.VEND.X263"], //investor name
+          row["Fields.352"], //investor #
+          row["Fields.364"], //loan number
+          row["Loan.BorrowerName"],
+          numeral(row["Loan.LoanAmount"]).format('$0,0'),
+          numeral(row["Fields.3"]).format('0.000'), //loan rate
+          numeral(row["Fields.4"]).format('000'), //loan term
+          row["Fields.362"], //loan processor
+          row["Fields.317"]
+        ];
+        // fields.forEach(function(col){
+        //   rowArray.push(row[col]);
+        // });
+        outputArray.push(rowArray);
+      });
+      //console.log(outputArray);
+      return outputArray;
+
+    }
 
   },
 
